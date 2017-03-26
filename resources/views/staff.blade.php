f4df4f0c11ab6578a327590430ae41f58dd7159e
John <jaboyak@gmail.com>
2017-03-26 01:14:49 -0700

    staff page updated

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -1,14 +1,33 @@
+@extends('layouts.index')
+@section('content')
 <h1 class="display-3">View & Edit Aces</h1>
-<h3>Alexander Hamilton Elementary</h3>
-<h4>Eliza Schuyler</h4>
+<h3>School name goes here</h3>
+<h4>Staff member name goes here</h4>
 <!-- Example row of columns -->
 <div class="row">
   <div class="col-md-6">
-    <h4>Aces Kids</h4>
+    <h4>Who have you been an ace to?</h4>
+    <form>
+      <fieldset>
+        <div class="form-group">
+          <div class="col-lg-4">
+            <input type="text" class="form-control" id="inputEmail" placeholder="ex: John Doe" autocomplete="off">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-lg-10 col-lg-offset-2">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+
     <ul>
-      <li>Aaron Burr</li>
-      <li>Hercules Mulligan</li>
-      <li>Lafayette</li>
+      <li>This is</li>
+      <li>the list</li>
+      <li>of their students</li>
+      <li>that they added</li>
     </ul>
   </div>
 </div>
+@endsection
