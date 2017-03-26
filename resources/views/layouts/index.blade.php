2113622908f02e20388c8f71ed680adb551a49bd
John <jaboyak@gmail.com>
2017-03-26 13:14:33 -0700

    updated navbar

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,7 +9,7 @@
     <title>Kids at Hope</title>
     <!-- Bootstrap core CSS -->
     <link href="http://bootswatch.com/readable/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
      <!--Custom styles for this template 
     <link href="./css/jumbotron.css" rel="stylesheet">
     <link href="./css/custom.css" rel="stylesheet">-->
@@ -25,7 +25,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/index"><img class="logo-image" alt="Kids at Hope Logo" src="img/site/logo.png" /></a>
+          <a class="navbar-brand" href="/homepage"><img class="logo-image" alt="Kids at Hope Logo" src="img/site/logo.png" /></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -2,94 +2,94 @@
 @section('content')
 
 <div class="container main">
-    <h1 class="display-3">View & Edit Aces</h1>
-        <h3>Alexander Hamilton Elementary</h3>
-        <h4>Eliza Schuyler</h4>
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="tabbable">
-            <ul class="nav nav-tabs">
-              <li class="active"><a href="#aces" data-toggle="tab"><h4>My Aces Students</h4></a></li>
-              <li><a href="#all" data-toggle="tab"><h4>View All Students</h4></a></li>
-            </ul>
-            <div class="tab-content">
+  <h1 class="display-3">View & Edit Aces</h1>
+  <h3>Alexander Hamilton Elementary</h3>
+  <h4>Eliza Schuyler</h4>
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="tabbable">
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#aces" data-toggle="tab"><h4>My Aces Students</h4></a></li>
+          <li><a href="#all" data-toggle="tab"><h4>View All Students</h4></a></li>
+        </ul>
+        <div class="tab-content">
 
-              <div class="tab-pane active" id="aces">
-                <ul class="list-unstyled student-list">
-                  <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
-                  <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
-                  <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
-                </ul>
-              </div>
-              
-              <div class="tab-pane" id="all">
-                <div class="student-search">
-                  <form class="form-inline">
-                    <div class="form-group inline-space">
-                      <label class="sr-only" for="studentName">Email address</label>
-                      <input type="text" class="form-control" id="studentName" placeholder="Search">
-                    </div>
-                    <select class="form-control inline-space">
-                      <option disabled>Filter by grade</option>
-                      <option>HeadStart</option>
-                      <option>Pre-K</option>
-                      <option>Kindergarten</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
-                      <option>7</option>
-                      <option>8</option>
-                      <option>9</option>
-                      <option>10</option>
-                      <option>11</option>
-                      <option>12</option>
-                    </select>
-                    <button type="submit" class="btn btn-primary inline-space">Search</button>
-                  </form>
-                </div>
-                <ul class="list-unstyled student-list">
-                  <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
-                  <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
-                  <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
-                  <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
-                  <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
-                  <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
-                  <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
-                  <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
-                  <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
-                  <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
-                  <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
-                  <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
-                </ul>
-                <nav aria-label="Page navigation">
-                  <ul class="pagination">
-                    <li>
-                      <a href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                      </a>
-                    </li>
-                    <li><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                    <li>
-                      <a href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </div>
+          <div class="tab-pane active" id="aces">
+            <ul class="list-unstyled student-list">
+              <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
+              <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
+              <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
+            </ul>
           </div>
-      
+          
+          <div class="tab-pane" id="all">
+            <div class="student-search">
+              <form class="form-inline">
+                <div class="form-group inline-space">
+                  <label class="sr-only" for="studentName">Email address</label>
+                  <input type="text" class="form-control" id="studentName" placeholder="Search">
+                </div>
+                <select class="form-control inline-space">
+                  <option disabled>Filter by grade</option>
+                  <option>HeadStart</option>
+                  <option>Pre-K</option>
+                  <option>Kindergarten</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10</option>
+                  <option>11</option>
+                  <option>12</option>
+                </select>
+                <button type="submit" class="btn btn-primary inline-space">Search</button>
+              </form>
+            </div>
+            <ul class="list-unstyled student-list">
+              <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
+              <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
+              <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
+              <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
+              <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
+              <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
+              <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
+              <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
+              <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
+              <li><span class="idnum">234</span><a href="student.html?aaronburr234">Aaron Burr</a></li>
+              <li><span class="idnum">768</span><a href="student.html?herculesmulligan768">Hercules Mulligan</a></li>
+              <li><span class="idnum">119</span><a href="student.html?lafayette119">Lafayette</a></li>
+            </ul>
+            <nav aria-label="Page navigation">
+              <ul class="pagination">
+                <li>
+                  <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                  </a>
+                </li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li>
+                  <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
+  
+    </div>
+  </div>
 </div>
 
 @endsection
